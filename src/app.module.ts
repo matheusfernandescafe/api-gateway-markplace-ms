dd08833608a01df22e9c5c945563cbcdaabb45cc
Matheus Fernandes <matheusfernandesofc@gmail.com>
2026-09-22 22:23:35 -0300

    feat: Configurações avançadas de CORS, Helmet e Rate Limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,19 @@ import { LoggingMiddleware } from './middleware/logging/logging.middleware.js';
     }),
     ThrottlerModule.forRoot([
       {
-        ttl: 60000,
-        limit: 100
+        name: 'short',
+        ttl: 1000, // 1 second
+        limit: 10, // 10 request per second
+      },
+      {
+        name: 'medium',
+        ttl: 60000, // 1 minute
+        limit: 100, //100 requests per minute
+      },
+      {
+        name: 'long',
+        ttl: 900000, // 15 minutes
+        limit: 1000, // 1000 requests per 15 minutes
       }
     ]),
     ProxyModule,
